f92e4d387325d23e4143aade3b109d93cd111461
kimJaeHyeon <kmr2644@gmail.com>
2022-07-30 21:40:49 +0900

    refactor01: Screening 은 Movie가 금액 할인 정책이나 비율할인 정책을 지원할 경우 isDiscountble 을 호출해 요금계산

--- src/object03/Screening.java
+++ src/object03/Screening.java
@@ -1,5 +1,7 @@
 package object03;
 
+import object02.Money;
+
 import java.time.LocalDateTime;
 
 public class Screening {
@@ -8,6 +10,28 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee()
+                                .times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee()
+                                .times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee()
+                            .times(audienceCount);
+        }
+        return movie.calculateNoneDiscountedFee()
+                    .times(audienceCount);
+    }
     public Movie getMovie() {
         return movie;
     }
